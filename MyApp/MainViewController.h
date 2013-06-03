597c95aa34e930b6d081212ddf73aff6a0909226
tranguyenthanh <trant@haloasia.net>
2013-06-03 19:24:50 +0700

    This is version 0.2

---
+++ MyApp/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  MyApp
+//
+//  Created by NTT on 6/3/13.
+//  Copyright (c) 2013 Halo Asia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
