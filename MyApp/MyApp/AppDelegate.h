617fb1f223143799ee7a39eb84f747b79347f7c3
tranguyenthanh <trant@haloasia.net>
2013-06-03 19:21:30 +0700

    This is version 1.0

---
+++ MyApp/MyApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyApp
+//
+//  Created by NTT on 6/3/13.
+//  Copyright (c) 2013 Halo Asia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
